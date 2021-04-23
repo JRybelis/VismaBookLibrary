9d22c75e0c14665ab7539db17560841c77fff992
JRybelis <72306551+JRybelis@users.noreply.github.com>
2021-04-23 11:51:02 +0300

    Add project files.

---
+++ Patron.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+
+namespace VismaBookLibrary
+{
+    class Patron
+    {
+        private string _name;
+        string Name
+        {
+            get => _name;
+            private set
+            {
+                Regex regex = new Regex(@"^[A-Za-Z ]+$");
+                if (regex.IsMatch(value))
+                {
+                    _name = value;
+                }
+                else
+                {
+                    Console.WriteLine("Please enter a valid name. /t Only Latin alphabet without special characters or digits allowed.");
+                }
+
+            }
+        }
+
+        private int _patronID;
+        public int PatronID
+        {
+            get => _patronID;
+            set
+            {
+                Regex regex = new Regex(@"^[0-9]+$");
+                if (regex.IsMatch(value.ToString()))
+                {
+                    _patronID = value;
+                }
+                else
+                {
+                    Console.WriteLine("Please enter a valid ID. /t Only digits are allowed.");
+                }
+            }
+        }
+    }
+}
